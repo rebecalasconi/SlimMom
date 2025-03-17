8420fd59ec3ec1a2253ee78222cb291497826ec0
rebecalasconi <rebecavoicilas@yahoo.com>
2025-03-17 21:02:20 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/SlimMom">
     <ReduxProvider>
       <App />
     </ReduxProvider>
